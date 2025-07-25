237e93f747022979454a30c53146367aa74d77e5
yongxianLiu <125457815+yongxianLiu@users.noreply.github.com>
2025-07-25 23:48:17 +0800

    Add files via upload

---
+++ BasicIRSTD-RRCANet/model/__init__.py
@@ -0,0 +1,18 @@
+from model.Unet.model_Unet import U_Net as Unet
+from model.ACM.model_ALCnet import ASKCResNetFPN as ALCNet
+from model.ACM.model_ACM import ASKCResUNet as ACM
+from model.DNANet.model_DNANet import DNANet as DNANet
+# from model.ISNet.model_ISNet import ISNet as ISNet
+from model.RISTDnet.model_RISTDnet import RISTDnet as RISTDnet
+from model.UIUNet.model_UIUNet import UIUNET as UIUNet
+from model.ISTDUNet.model_ISTDUNet import ISTDU_Net as ISTDU_Net
+from model.RDIAN.model_RDIAN import RDIAN as RDIAN
+from model.ResUNet.model_ResUNet import ResUNet as ResUNet
+from model.AMFU.AMFU import AMFU as AMFU
+# from model.AMFU.AMFU_RuCB import AMFU_RuCB as AMFU_RuCB
+from model.RRCANet.model_RRCANet import RRCANet as RRCANet
+from model.ResUNet.ResUNet_RuCB import ResUNet_RuCB as ResUNet_RuCB
+# from model.DNANet.DNANet_RuCB import DNANet_RuCB as DNANet_RuCB
+from model.LW_IRSTNet.LW_IRSTNet import LW_IRST_ablation as LW_IRSTNet
+# from model.RepISDNet.RepISD import RepISD as RepISDNet
+# from model.RepISDNet.convert_CDC import *

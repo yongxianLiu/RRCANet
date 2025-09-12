6b1bc85bb78098d1674518910c0de76b010eddc3
yongxianLiu <125457815+yongxianLiu@users.noreply.github.com>
2025-09-12 09:56:40 +0800

    Update complexity.py

--- RRCA-Net/model/complexity.py
+++ RRCA-Net/model/complexity.py
@@ -13,19 +13,13 @@ from model.ResUNet_RuCB import *
 from model.model_res_UNet import *
 from model.AMFU import *
 
-
-args = parse_args()
-nb_filter, num_blocks = load_param(args.channel_size, args.backbone)
-
 net = AMFU()
-# net = AMFU_RuCB(n_classes=1,in_channels=3,iterations=2, multiplier=1.0, num_layers=5, integrate='True', deep_supervision='True')
 #net = DNANet(num_classes=1, input_channels=3, nb_filter=[16,32,64,128,256], num_blocks=[2,2,2,2], block=Res_CBAM_block, deep_supervision=True)
 # net = res_UNet(num_classes=1, input_channels=3, block=Res_block, num_blocks=[2,2,2,2],nb_filter=[8,16,32,64,128])
-# net = RRCANet(num_classes=1, input_channels=3, block=Res_block, num_blocks=num_blocks, iterations=2, num_layers=4,
+# net = RRCANet(num_classes=1, input_channels=3, block=Res_block, num_blocks=[3,2,2,2], iterations=2, num_layers=4,
 #                    multiplier=1.0, integrate='True', deep_supervision='False')
 # net = ResUNet_RuCB(num_classes=1, input_channels=3, block=Res_block, num_blocks=num_blocks, iterations=2, num_layers=5,
 #                    multiplier=1.0, integrate='True', deep_supervision='False')
-# net = res_UNet(num_classes=1, input_channels=3, block=Res_block, num_blocks=[2,2,2,2], nb_filter=nb_filter)
 
 
 timef=0
@@ -47,6 +41,7 @@ print(out[0].size())
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 print('parameters_count:',count_parameters(net))
+
 
 
 
